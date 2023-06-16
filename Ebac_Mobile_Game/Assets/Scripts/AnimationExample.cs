1f829ccdac4089fcaa9503b9a71e6fc4887987bc
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-06-16 15:30:03 +0100

    Animator Manager and Parameters

---
+++ Ebac_Mobile_Game/Assets/Scripts/AnimationExample.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationExample : MonoBehaviour
+{
+    public Animation animation;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayAnimation(run);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayAnimation(idle);
+
+        }
+    }
+
+public void PlayAnimation(AnimationClip c)
+{
+    // Play on animation
+    /*
+    animation.clip = c;
+    animation.Play();*/
+
+
+    //or
+
+    //Use CrossFade to make a smooth transition
+    animation.CrossFade(c.name);
+
+}
+
+}
+
