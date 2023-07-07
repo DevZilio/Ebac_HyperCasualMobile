f6ba1d2abdb7d03b746547ec30f1c6d37f95bbbe
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-07-07 10:06:46 +0100

    KeyBoard controller added to tests

--- Ebac_Mobile_Game/Assets/Scripts/TouchController.cs
+++ Ebac_Mobile_Game/Assets/Scripts/TouchController.cs
@@ -9,23 +9,37 @@ public class TouchController : MonoBehaviour
     public Vector2 pastPosition;
     public float velocity = 1f;
 
+    // public void Update()
+    // {
+
+
+    //     if (Input.GetMouseButton(0))
+    //     {
+    //         //MousePosition AGORA - mousePosition PASSADO
+    //         Move(Input.mousePosition.x - pastPosition.x);
+    //     }
+    //     pastPosition = Input.mousePosition;
+    // }
+
+
+    // public void Move(float speed)
+    // {
+    //     {
+    //         transform.position += Vector3.right * Time.deltaTime * speed * velocity;
+    //     }
+    // }
+
+   public float speed = 1f;
+
     public void Update()
     {
-
-
-        if (Input.GetMouseButton(0))
-        {
-            //MousePosition AGORA - mousePosition PASSADO
-            Move(Input.mousePosition.x - pastPosition.x);
-        }
-        pastPosition = Input.mousePosition;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Move(horizontalInput);
     }
 
-
-    public void Move(float speed)
+    public void Move(float direction)
     {
-        {
-            transform.position += Vector3.right * Time.deltaTime * speed * velocity;
-        }
+        transform.position += Vector3.right * Time.deltaTime * direction * speed;
     }
+
 }
