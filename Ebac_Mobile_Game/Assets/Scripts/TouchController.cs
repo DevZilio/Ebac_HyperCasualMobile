40a450554efd074c348c37b59352489bddb8577e
DevZilio <105543270+DevZilio@users.noreply.github.com>
2023-07-24 19:54:49 +0100

    Touch Controller improved
    - new code to listen finger touch and responsive

--- Ebac_Mobile_Game/Assets/Scripts/TouchController.cs
+++ Ebac_Mobile_Game/Assets/Scripts/TouchController.cs
@@ -1,45 +1,35 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-
 using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    public Vector2 pastPosition;
-    public float velocity = 3f;
+    private Vector2 initialTouchPosition;
+    private bool isMoving = false;
+    public float moveSpeed = 10f; // Define a velocidade de movimento do personagem.
+    public float minX; // Defina o valor mínimo da posição X permitida para o personagem.
+    public float maxX; // Defina o valor máximo da posição X permitida para o personagem.
 
     public void Update()
     {
-
-
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount > 0)
         {
-            //MousePosition AGORA - mousePosition PASSADO
-            Move(Input.mousePosition.x - pastPosition.x);
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                initialTouchPosition = touch.position;
+                isMoving = true;
+            }
+            else if (touch.phase == TouchPhase.Moved && isMoving)
+            {
+                Vector2 touchDelta = touch.deltaPosition;
+                float targetX = transform.position.x + touchDelta.x * moveSpeed * Time.deltaTime;
+                float clampedX = Mathf.Clamp(targetX, minX, maxX);
+                transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
+            }
+            else if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                isMoving = false;
+            }
         }
-        pastPosition = Input.mousePosition;
     }
-
-
-    public void Move(float speed)
-    {
-        {
-            transform.position += Vector3.right * Time.deltaTime * speed * velocity;
-        }
-    }
-
-//    public float speed = 1f;
-
-//     public void Update()
-//     {
-//         float horizontalInput = Input.GetAxis("Horizontal");
-//         Move(horizontalInput);
-//     }
-
-//     public void Move(float direction)
-//     {
-//         transform.position += Vector3.right * Time.deltaTime * direction * speed;
-//     }
-
 }
